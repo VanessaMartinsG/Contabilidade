7bbecd3d0bab618c2a6b69eb3cd072afbb5ddf5f
lazaroq11 <lazaroq11@gmail.com>
2022-03-26 11:07:31 -0300

    fix:add cpf mask

--- src/js/maskCpf.js
+++ src/js/maskCpf.js
@@ -5,13 +5,10 @@
    cpf.addEventListener('keypress',()=>{
       let cpfLength = cpf.value.length;
       
-      
       if(isNaN(cpf.value[cpfLength-1])){
          cpf.value = cpf.value.substring(0, cpfLength-1); 
          return false;
       }
-
-
 
       if(cpfLength === 3 || cpfLength === 7){
          cpf.value += "." 
