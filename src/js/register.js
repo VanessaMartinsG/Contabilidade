6dd934a0004ed6499c42474248ab2ef289671ce3
lazaroq11 <lazaroq11@gmail.com>
2022-04-01 19:12:43 -0300

    feat:add border red when field is empty

--- src/js/register.js
+++ src/js/register.js
@@ -1,7 +1,7 @@
 (() => {
 const baseURL = "http://localhost:3000";
-const form_login =  document.forms.register__box__form
-console.log(form_login);
+
+
 form_login.addEventListener("submit",(e)=>{
     e.preventDefault();
     const{name,email,cpf,saldo} = form_login;
@@ -11,9 +11,57 @@ form_login.addEventListener("submit",(e)=>{
         "cpf":cpf.value,
         "saldo":saldo.value
     };
-
+    let validaCampos = validaCampo(name,email,cpf,saldo);
+    if(validaCampos === false){
     createUser(users);
+    }
+
 })
+
+
+function validaCampo(name,email,cpf,saldo){
+    const nomeInpt = document.getElementById("name");
+    const emailInpt = document.getElementById("email");
+    const cpfInpt = document.getElementById("cpf");
+    const saldoInpt = document.getElementById("saldo");
+    let valida = true;
+
+    if(name.value == "" || email.value == "" || cpf.value == "" || saldo.value == "" ){
+        if(name.value == ""){
+            nomeInpt.style.border = "1px solid red";
+        }
+        else{    
+            nomeInpt.style.border = "1px solid #D7D7D7";
+        }
+
+        if(email.value == ""){
+            emailInpt.style.border = "1px solid red"; 
+        }
+        else{
+            emailInpt.style.border = "1px solid #D7D7D7";
+        }
+
+        if(cpf.value == ""){
+            cpfInpt.style.border = "1px solid red"; 
+        }
+        else{ 
+            cpfInpt.style.border = "1px solid #D7D7D7"; 
+        }
+
+        if(saldo.value == ""){
+            saldoInpt.style.border = "1px solid red";  
+        }
+      else{
+            saldoInpt.style.border = "1px solid #D7D7D7";
+        }
+
+    }else{
+        valida = false;
+    }
+   
+        return valida;
+}
+
 
 async function createUser(users){
     let response = await fetch(`${baseURL}/users`,{
